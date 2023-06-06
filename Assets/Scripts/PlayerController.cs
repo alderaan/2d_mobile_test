813a3c9bda4f4ee541d4c7ebe62e208db0b4603f
David Lucker <david.l@gojek.com>
2023-06-07 00:52:06 +0700

    changes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,6 +14,12 @@ public class PlayerController : MonoBehaviour
     private float verticalInput;
     private CameraController.CameraBounds bounds;
 
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -22,18 +28,22 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
         constantUpwardSpeed = camContr.currentSpeed;
-    
+
         // Calculate the new position of the ship
         Vector3 newPosition = transform.position + new Vector3(
             horizontalInput * moveSpeed * Time.deltaTime, 
             constantUpwardSpeed * Time.deltaTime + verticalInput * moveSpeed * Time.deltaTime,
-             0
-             );
+            0
+            );
 
         // Make sure the new position isn't outside the camera's bounds
         bounds = CameraController.GetCameraBounds(camContr.GetComponent<Camera>());
-        newPosition.x = Mathf.Clamp(newPosition.x, bounds.BottomLeft.x, bounds.TopRight.x);
-        newPosition.y = Mathf.Clamp(newPosition.y, bounds.BottomLeft.y, bounds.TopRight.y);
+
+        float halfPlayerWidth = spriteRenderer.bounds.extents.x;
+        float halfPlayerHeight = spriteRenderer.bounds.extents.y;
+
+        newPosition.x = Mathf.Clamp(newPosition.x, bounds.BottomLeft.x + halfPlayerWidth, bounds.TopRight.x - halfPlayerWidth);
+        newPosition.y = Mathf.Clamp(newPosition.y, bounds.BottomLeft.y + halfPlayerHeight, bounds.TopRight.y - halfPlayerHeight);
 
         // Move the ship to the new position
         transform.position = newPosition;
